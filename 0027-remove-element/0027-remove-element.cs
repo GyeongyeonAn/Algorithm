aa97751d0f63096af443488839eb9ebd3e0e95d6
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-10-19 23:28:19 +0900

    Time: 130 ms (44.82%), Space: 42.7 MB (85.75%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int RemoveElement(int[] nums, int val) 
+    {
+        List<int> removeIndex = new List<int>();
+        int n = nums.Length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (nums[i] == val) {
+                removeIndex.Add(i);
+            }
+        }
+
+        foreach (int index in removeIndex) {
+            for (int j = index; j < n - 1; j++) {
+                nums[j] = nums[j + 1];
+            }
+            n--;
+        }
+
+        return n;
+    }
+}
