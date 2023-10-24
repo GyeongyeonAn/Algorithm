053630eab70d06ed73e9bd95d7f38b5ea7c1d8c0
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-10-24 15:02:04 +0900

    Time: 91 ms (78.05%), Space: 52 MB (90.09%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cs
@@ -2,9 +2,8 @@ public class Solution {
     public double FindMedianSortedArrays(int[] nums1, int[] nums2) 
     {
         // 입력 배열 중 nums1이 더 작은 배열인 경우, 코드를 간단히 하기 위해 두 배열을 교환합니다.
-        if (nums1.Length > nums2.Length) {
+        if (nums1.Length > nums2.Length) 
             return FindMedianSortedArrays(nums2, nums1);
-        }
         
         int x = nums1.Length;  // 배열 nums1의 길이
         int y = nums2.Length;  // 배열 nums2의 길이
@@ -24,22 +23,14 @@ public class Solution {
             
             if (maxX <= minY && maxY <= minX) {
                 if ((x + y) % 2 == 0) 
-                {
                     return (Math.Max(maxX, maxY) + Math.Min(minX, minY)) / 2.0;
-                } 
                 else 
-                {
                     return Math.Max(maxX, maxY);
-                }
             } 
             else if (maxX > minY) 
-            {
                 high = partitionX - 1;
-            } 
             else 
-            {
                 low = partitionX + 1;
-            }
         }
         
         // 오류 발생 시 기본값 반환.
