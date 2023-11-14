a84bcf682aa1290e68d1d62f6aa553b9f410048a
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-11-14 17:26:11 +0900

    Time: 79 ms (23.84%), Space: 46.5 MB (19.66%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cs
@@ -0,0 +1,35 @@
+public class Solution 
+{
+    public string IntToRoman(int num) 
+    {
+        Dictionary<int, string> roman = new Dictionary<int, string>()
+        {
+            {1000, "M"},
+            {900, "CM"},
+            {500, "D"},
+            {400, "CD"},
+            {100, "C"},
+            {90, "XC"},
+            {50, "L"},
+            {40, "XL"},
+            {10, "X"},
+            {9, "IX"},
+            {5, "V"},
+            {4, "IV"},
+            {1, "I"},
+        };
+        
+        StringBuilder result = new StringBuilder();
+
+        foreach (var pair in roman)
+        {
+            while (num >= pair.Key)
+            {
+                result.Append(pair.Value);
+                num -= pair.Key;
+            }
+        }
+
+        return result.ToString();
+    }
+}
