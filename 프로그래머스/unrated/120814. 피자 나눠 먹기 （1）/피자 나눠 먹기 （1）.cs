d29c5538eb10913391df8d09d9bb7e82e9a14f38
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-11-16 17:37:09 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.15 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        int answer = 0;
+        
+        answer = n%7 == 0 ? n/7 : n/7 + 1;
+        
+        return answer;
+    }
+}
