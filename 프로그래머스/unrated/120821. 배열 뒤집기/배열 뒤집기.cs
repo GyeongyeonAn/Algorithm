92a0494ed1601706b6c121a4f140b6dba3a408bb
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-11-16 17:21:29 +0900

    [level unrated] Title: 배열 뒤집기, Time: 0.16 ms, Memory: 30.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120821. 배열 뒤집기/배열 뒤집기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int[] num_list) 
+    {
+        int length = num_list.Length;
+        int[] answer = new int[length];
+        
+        for(int i=0; i<length; i++)
+        {
+            answer[i] = num_list[length-i-1];
+        }
+        
+        return answer;
+    }
+}
