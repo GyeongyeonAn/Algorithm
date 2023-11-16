7c175f3189a23aee767fe6718655b2b0f354d756
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-11-16 17:11:04 +0900

    [level unrated] Title: 양꼬치, Time: 0.21 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) 
+    {
+        int answer = 0;
+        int nPrice = n * 12000;
+        int kPrice = (k - (n/10)) * 2000;
+        answer = nPrice + kPrice;
+        return answer;
+    }
+}
