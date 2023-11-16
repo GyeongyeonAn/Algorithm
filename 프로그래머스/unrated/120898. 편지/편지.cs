087e2433c3c9ffaecdb0136dacf8ccee952991ea
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-11-16 17:13:20 +0900

    [level unrated] Title: 편지, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120898. 편지/편지.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution 
+{
+    public int solution(string message) 
+    {
+        int answer = 0;
+        answer = 2 * message.Length;
+        return answer;
+    }
+}
