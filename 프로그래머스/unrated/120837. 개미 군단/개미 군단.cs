ccdfb2056f0e31e5c78e2214c34407cf0bd92da7
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-11-21 20:18:13 +0900

    [level unrated] Title: 개미 군단, Time: 0.17 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int hp) 
+    {
+        int answer = 0;
+        
+        while(hp > 0)
+        {
+            if(hp - 5 >= 0)
+            {
+                hp -= 5;
+                answer++;
+            }
+            else if(hp - 3 >= 0)
+            {
+                hp -= 3;
+                answer++;
+            }
+            else if(hp - 1 >= 0)
+            {
+                hp -= 1;
+                answer++;
+            }
+            else
+                break;
+        }
+        
+        return answer;
+    }
+}
