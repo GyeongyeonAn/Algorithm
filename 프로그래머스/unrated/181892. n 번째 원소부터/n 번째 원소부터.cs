3eeb7d542be9edcbd28a8a15f82d23fc583c95bf
안경연 <55589616+GyeongyeonAn@users.noreply.github.com>
2023-12-05 21:17:19 +0900

    [level unrated] Title: n 번째 원소부터, Time: 0.57 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public int[] solution(int[] num_list, int n) 
+    {
+        List<int> result = new List<int>();
+        for(int i=n-1; i<num_list.Length; i++)
+        {
+            result.Add(num_list[i]);
+        }
+        
+        int length = result.Count;
+        int[] answer = new int[length];
+        for(int i=0; i<length; i++)
+            answer[i] = result[i];
+        return answer;
+    }
+}
